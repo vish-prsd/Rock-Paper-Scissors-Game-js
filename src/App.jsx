b8d158001ce2e8d218b8939d3b3712e80511b7aa
vish-prsd <psvishnu286@gmail.com>
2025-01-12 15:44:47 +0530

    updated the file to react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RockPaperScissors from "./RockPaperScissors";
+
+const App = () => {
+  return (
+    <div>
+      <RockPaperScissors />
+    </div>
+  );
+};
+
+export default App;
